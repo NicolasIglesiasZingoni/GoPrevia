3a8fde2c808fb6c8f080f84075acae3df7f9347f
NicolasIglesiasZingoni <nicolasiglesiaszingoni@gmail.com>
2015-04-26 02:34:22 -0300

    Agrego avances en welcom-view
    Agrego lo que avance hasta el momento con el body.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,127 @@
 	<head>
 		<title>Laravel</title>
 		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+		<link rel='stylesheet' href="{{ asset('/css/welcome.css') }}">
 	</head>
 	<body>
 			<nav>
-				
-				
+			<!--AGUS ADENTRO DE ESTA ETIQUETA TENES QUE TRABAJAR VOS, LO DEMAS NO LO TENES QUE CAMBIAR
+			POR QUE ESTAMOS TRABAJANDO NAY Y YO  -->
 			</nav>
 
 
-		<footer>
-			
+				<div class="conte-principal">
+					<div>					
+						<div class="col-conte-1">
+							<div >
+								<h1 class="title-MPT">Música para todos.</h1>
+								<p class="parraf-conte-1">GoPrevia es toda la música que necesitarás.</p>
+								<p class="parraf-conte-1 parraf2">Escucha gratis o suscríbete a GoPrevia Premium.</p>								
+							</div>
+								<a href="" class="btn-lst-gratis">
+									ESCUCHA GRATIS								
+								</a>
+								<p class="separador-btn" id="p1">
+								<span>O</span>
+								</p>
+								<a href="" class="btn-premium">
+									CONTRATA PREMIUM
+								</a>
+								<p class="parraf-small">
+									Reproduce canciones al instante, escucha sin conexión y sin anuncios.
 
+								</p>
+								<p class="parraf-small">
+									Toma la prueba gratuita.
+								</p>
+						</div>
+						<div class="col2-conte-1">
+						<!-- IMAGEN DEL CELULAR BLANCO PRIMER PANTALLA -->
+						</div>
+					</div>
+					<div id="conte-row-2">
+						<div class="col-conte-2">
+							<div>
+								<h1 class="title-col-1">Reproduce la música que te encanta.</h1>
+								<p class="parraf-conte-2">Escucha a tus artistas favoritos, échales un vistazo a los nuevos lanzamientos
+								 o reproduce las canciones que te hacen recordar las buenas épocas.</p>
+							</div>
+							<div>
+								
+							</div>
+						</div>
+
+						<div class="col2-conte-2">
+							<div class="row-col2">
+								<!-- IMAGEN  -->
+							</div>
+
+							<div class="row2-col2">
+								<h1 class="title-row2">Déjanos entretenerte.</h1>
+								<p class="parraf-row2">Cada vez que enciendas Spotify, 
+								te sugeriremos música increíble para el momento.								
+								</p>
+							</div>
+						</div>
+						<div class="col3-conte-2">
+							
+
+						</div>
+					</div>
+					
+					<div class="conte-row-3">
+						<div class="col-row-3">
+							<h1 class="title-row-3">Déjate llevar por el momento.</h1>
+							<p class="parraf-row-3">Sin importar lo que estés haciendo o cómo te estés sintiendo, 
+							nosotros encontraremos playlists que coincidan a la perfección con tu estado de ánimo.</p>
+							<a href="" class="btn-gratis-row-3">ESCUCHA GRATIS</a>
+						</div>
+						<div class="col2-row-3">
+							
+						</div>
+					</div>						
+						<div class="conte-row-4">
+							<h1 class="title-row-4">  
+								Escucha 
+								<strong>gratis</strong>
+								 o sucríbete a 
+								<strong>GoPrevia Premium.</strong>
+								
+							</h1>
+						</div>
+						<div id="conte-row-5">						
+						<div class="col-row-5">
+							<h2 class="title-col">Esto es GoPrevia Gratis</h2>	
+							<ul class="list-col">
+								<li>Reproduce en modo aleatorio cualquier playlist o artista en tus dispositivos móviles.</li>
+								<li>Hecho posible gracias a los anuncios.</li>
+								<li>Sáltate seis canciones por hora en tus dispositivos móviles.</li>
+							</ul>
+							<a href="" class="btn-col-gratis">ESCUCHA GRATIS</a>
+						</div>
+						
+
+						<div class="col2-row-5">
+							<h2 class="title-col2">Esto es Spotify Premium</h2>
+							<small class="small-col2">Pruébalo gratis durante 30 días</small>
+							<ul class="lst-col2">
+								<li>Reproduce canciones a pedido en tus dispositivos móviles.</li>
+								<li>Escucha sin conexión. No se necesita una conexión a Internet..</li>
+								<li>Saltos de canciones ilimitados.</li>
+								<li>Mejor calidad de sonido.</li>
+								<li>Reproduce Spotify en los altavoces de tu casa.</li>
+							</ul>
+							<a href="" class="btn-col2-premium">CONTRATA PREMIUM</a>
+
+						</div>
+					</div>
+				</div>
+
+
+
+		<footer>
+		<!-- NAY ACA ADENTRO DE ESTAS ETIQUETAS FOOTER TENES QUE TRABAJAR VOS Y ARMAR TU FOOTER, 
+		LO DE ARRIBA NO LO TENES QUE ALTERAR SIN AVISAR -->
 		</footer>
 	</body>
 </html>
